3bc11be6cc6b02818d8e074ae5ba0c7c99f1494c
POFFIOLEG <144817552+rngblessed@users.noreply.github.com>
2024-06-04 02:49:18 +0300

    sdvsdcbf

--- edit_user.php
+++ edit_user.php
@@ -12,7 +12,7 @@ $db = new PDO(
 // Проверка, был ли отправлен запрос на обновление данных
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['user_id'];
-    $fullName = $_POST['Name'];
+    $Name = $_POST['Name'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
     $birthDate = $_POST['birth_date'];
